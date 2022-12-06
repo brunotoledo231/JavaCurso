d70739e2cbdf94b8d3507f57ed587a53f0d5d333
bruno toledo <vrunotoledo@gmail.com>
2022-12-06 08:16:54 -0300

    subiendo ejercicio 1

---
+++ src/Ejercicio1/Tipos.java
@@ -0,0 +1,30 @@
+package Ejercicio1;
+
+public class Tipos {
+    public static void main(String[] args) {
+        int numero=1;
+        byte numero2=2;
+        short numero3=3;
+        long numero4=4;
+        float decimal1=1.23f;
+        double decimal2=2.34;
+        char caracter1='a';
+        boolean v=true;
+        boolean f=false;
+        String cadena1="Hola Soy Bruno";
+        Integer numero5=null;
+        System.out.println(numero);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+        System.out.println(caracter1);
+        System.out.println(v);
+        System.out.println(f);
+        System.out.println(cadena1);
+
+
+    }
+}
